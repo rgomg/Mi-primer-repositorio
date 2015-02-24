b9574772d8072b86313abf641b4bfe6afc23cd50
rgomg <jruben_gomez@hotmail.com>
2015-02-24 08:59:41 -0600

    * HolaMundo.sln: * HolaMundo.userprefs: * HolaMundo.Android.csproj: * Resource.designer.cs:
    * App.cs:
    * repositories.config: Actividad realizada

--- HolaMundo/HolaMundo/App.cs
+++ HolaMundo/HolaMundo/App.cs
@@ -9,7 +9,7 @@ namespace HolaMundo
 		{	
 			return new ContentPage { 
 				Content = new Label {
-					Text = "Hola mundo!",
+					Text = "Nombre: JOSE RUBEN GOMEZ GAYTAN - MATRICULA: 413103446",
 					VerticalOptions = LayoutOptions.CenterAndExpand,
 					HorizontalOptions = LayoutOptions.CenterAndExpand,
 				},
