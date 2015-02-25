0a514246b68040115a6103fafb4d9b4960d60167
rgomg <jruben_gomez@hotmail.com>
2015-02-25 10:58:25 -0600

    Actividad completada

--- Actividad4/Actividad4/App.cs
+++ Actividad4/Actividad4/App.cs
@@ -6,28 +6,20 @@ namespace Actividad4
 	public class App
 	{
 		public static Page GetMainPage ()
-		{	
-			//Se crea un objeto etiqueta
-			Label label = new Label();
+		{    
+			Label label = new Label{
 
-			//Se modifican sus propiedades
-			//Texto
-			label.Text = "Cámbiame";
-			//Color del texto
-			label.TextColor = Color.Blue;
-			//Alineación vertical (YAlign). Para la alineación horizontal, usar XAlign
-			label.YAlign = TextAlignment.Center;
+				Text = "Utilizando inicializadores de objetos en lugar de propiedades",
+				TextColor = Color.Red,
+				YAlign = TextAlignment.Center};
 
-
-			//El mismo código usando inicializador de objetos
-			//Label label = new Label{ Text = "Cámbiame", TextColor =  Color.Blue, YAlign = TextAlignment.Center };
-
-			//Se crea una página y se le asigna como contenido la eqtiqueta que se creó
 			ContentPage contentPage = new ContentPage();
 			contentPage.Content = label;
 
 			return contentPage;
+
 		}
 	}
+
 }
 
