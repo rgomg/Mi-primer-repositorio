ab0ebe2ba34c48070d36eadf44e52354a4dab67e
rgomg <jruben_gomez@hotmail.com>
2015-03-10 14:42:36 -0600

    Actividad realizada

--- Actividad7/Actividad7/App.cs
+++ Actividad7/Actividad7/App.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Xamarin.Forms;
-// using Xamarin.Forms;
 
 namespace Actividad7
 {
--- Actividad7/Actividad7/Pagina2.cs
+++ Actividad7/Actividad7/Pagina2.cs
@@ -3,39 +3,31 @@ using Xamarin.Forms;
 
 namespace Actividad7
 {
-	public class Pagina2: ContentPage
-	{
-		public Pagina2 ()
-		{
-			Label texto = new Label {
-				Text = "Página 2",
-				TextColor = Color.White
-			};
+    public class Pagina2: ContentPage
+    {
+        public Pagina2 ()
+        {
+            Label texto = new Label {
+                Text = "Página 2",
+                TextColor = Color.Green
+            };
 
-			Button boton = new Button
-			{
-				Text = "Click para navegar a la página 3"
-			};
+            //Stacklayout permite apilar los controles verticalmente
+            StackLayout stackLayout = new StackLayout
+            {
+                Children =
+                {
+                    texto
 
-			boton.Clicked += (sender, e) => {
-				this.Navigation.PushAsync(new Pagina3());
-			};
-
-			//Stacklayout permite apilar los controles verticalmente
-			StackLayout stackLayout = new StackLayout
-			{
-				Children =
-				{
-					texto,
-					boton
-				}
-				};
+                }
+            };
 
 
-			//Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
-			this.Content = stackLayout;
-			this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
-		}
-	}
+            //Como esta clase hereda de ContentPage, podemos usar estas propiedades directamente
+            this.Content = stackLayout;
+            this.Padding = new Thickness (5, Device.OnPlatform (20, 5, 5), 5, 5);
+        }
+    }
 }
+
 
